c1c8a31a98bab65d1d1fbda92da02bdfee0267f9
kesshei <dagang520@qq.com>
2023-06-12 20:26:36 +0800

    新增项目

---
+++ VirtualMachineDemo/VirtualMachine_2/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace VirtualMachine_2
+{
+    internal class Program
+    {
+        /// <summary>
+        /// 虚拟机2，实现更多指令
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
