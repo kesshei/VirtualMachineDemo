799c159c352be75765fecef43a22d7189631bef3
kesshei <dagang520@qq.com>
2023-09-16 13:04:41 +0800

    提交

--- VirtualMachineDemo/LC_3/Program.cs
+++ VirtualMachineDemo/LC_3/Program.cs
@@ -4,6 +4,8 @@
     {
         static void Main(string[] args)
         {
+            bool a = false;
+ 
             //VM.LoadAsm(File.ReadAllLines(@"C:\Users\Administrator\Desktop\test\test2\test2.asm"));
             VM.LoadBin(File.ReadAllLines(@"C:\Users\Administrator\Desktop\test\新建文件夹\123.bin"));
             Console.WriteLine("LC_3 !");
